0e09a4ec410b59a545982f501b0c8b2d598dee40
Eric Reed <mrericcoolnessreed@gmail.com>
2021-07-16 15:25:50 -0400

    fixed varint/varlong max size checks

--- LayerOne/types/varint.py
+++ LayerOne/types/varint.py
@@ -3,7 +3,7 @@ from LayerOne.network.conn_wrapper import ConnectionWrapper
 
 class _VarBase:
     @staticmethod
-    def read (conn_wrapper: ConnectionWrapper, max_read: int, bit_count: int, name: str) -> (int, int):
+    def read (conn_wrapper: ConnectionWrapper, max_bits: int, name: str) -> (int, int):
         read_count: int = 0
         result: int = 0
         _input: int = 0
@@ -13,14 +13,14 @@ class _VarBase:
             result |= (value << (7 * read_count))
 
             read_count += 1
-            if read_count > max_read: raise ProtocolException (f"{name} is too big")
+            if result >> max_bits > 0: raise ProtocolException (f"{name} is too big")
             if _input & 0b10000000 == 0: break
         result_signed = switch_format ("@I", "<i", result)
         return result_signed, read_count
     @staticmethod
-    def write (conn_wrapper: ConnectionWrapper, value: int, max_written: int, bit_count: int, name: str) -> None:
+    def write (conn_wrapper: ConnectionWrapper, value: int, max_bits: int, name: str) -> None:
         value_unsigned = switch_format ("<i", "@I", value)
-        if value_unsigned >> bit_count > 0: raise ProtocolException (f"{name} is too big")
+        if value_unsigned >> max_bits > 0: raise ProtocolException (f"{name} is too big")
         while True:
             temp: int = value_unsigned & 0b01111111
             value_unsigned >>= 7
@@ -30,14 +30,19 @@ class _VarBase:
             if value_unsigned == 0:
                 break
 
+_BITS_PER_BYTE = 8
+# these are because in java, varints and varlongs decompress to their associated native types, which have max sizes of 4 and 8 bytes respectively
+_VARINT_MAX_BITS = _BITS_PER_BYTE * 4
+_VARLONG_MAX_BITS = _BITS_PER_BYTE * 8
+
 class VarInt:
     @staticmethod
-    def read (conn_wrapper: ConnectionWrapper): return _VarBase.read (conn_wrapper = conn_wrapper, max_read = 7, bit_count = 32, name = "VarInt")
+    def read (conn_wrapper: ConnectionWrapper): return _VarBase.read (conn_wrapper = conn_wrapper, max_bits = _VARINT_MAX_BITS, name = "VarInt")
     @staticmethod
-    def write (conn_wrapper: ConnectionWrapper, value: int): return _VarBase.write (conn_wrapper = conn_wrapper, value = value, max_written = 7, bit_count = 32, name = "VarInt")
+    def write (conn_wrapper: ConnectionWrapper, value: int): return _VarBase.write (conn_wrapper = conn_wrapper, value = value, max_bits = _VARINT_MAX_BITS, name = "VarInt")
 
 class VarLong:
     @staticmethod
-    def read (conn_wrapper: ConnectionWrapper): return _VarBase.read (conn_wrapper = conn_wrapper, max_read = 7, bit_count = 32, name = "VarLong")
+    def read (conn_wrapper: ConnectionWrapper): return _VarBase.read (conn_wrapper = conn_wrapper, max_bits = _VARLONG_MAX_BITS, name = "VarLong")
     @staticmethod
-    def write (conn_wrapper: ConnectionWrapper, value: int): return _VarBase.write (conn_wrapper = conn_wrapper, value = value, max_written = 7, bit_count = 32, name = "VarInt")
+    def write (conn_wrapper: ConnectionWrapper, value: int): return _VarBase.write (conn_wrapper = conn_wrapper, value = value, max_bits = _VARLONG_MAX_BITS, name = "VarInt")
